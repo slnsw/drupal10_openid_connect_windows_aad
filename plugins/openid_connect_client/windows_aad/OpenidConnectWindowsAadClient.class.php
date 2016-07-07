c7a5063cee17296ae419ffb9e0cdfc7b4d4af7e9
Tom van Vliet <tom.van.vliet@finalist.nl>
2016-07-07 13:08:15 +0200

    Add option to retrieve userinfo by making use of the graph api

--- plugins/openid_connect_client/windows_aad/OpenidConnectWindowsAadClient.class.php
+++ plugins/openid_connect_client/windows_aad/OpenidConnectWindowsAadClient.class.php
@@ -33,6 +33,12 @@ class OpenidConnectWindowsAadClient extends OpenIDConnectClientBase {
       '#type' => 'textfield',
       '#default_value' => $this->getSetting('userinfo_endpoint_wa', $default_site . '/openid/userinfo'),
     );
+    $form['userinfo_graph_api_wa'] = array(
+      '#title' => t('Use Graph API for user info'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('userinfo_graph_api_wa'),
+      '#description' => t('This option will omit the Userinfo endpoint and will use the Graph API ro retrieve the userinfo.'),
+    );
 
     return $form;
   }
@@ -50,11 +56,66 @@ class OpenidConnectWindowsAadClient extends OpenIDConnectClientBase {
   }
 
   /**
+   * Overrides OpenIDConnectClientInterface::retrieveIDToken().
+   */
+  public function retrieveTokens($authorization_code) {
+    // Exchange `code` for access token and ID token.
+    $redirect_uri = OPENID_CONNECT_REDIRECT_PATH_BASE . '/' . $this->name;
+    $post_data = array(
+      'code' => $authorization_code,
+      'client_id' => $this->getSetting('client_id'),
+      'client_secret' => $this->getSetting('client_secret'),
+      'redirect_uri' => url($redirect_uri, array('absolute' => TRUE)),
+      'grant_type' => 'authorization_code',
+    );
+
+    // Add Graph API as resource if option is set.
+    if ($this->getSetting('userinfo_graph_api_wa') == 1) {
+      $post_data['resource'] = 'https://graph.windows.net';
+    }
+
+    $request_options = array(
+      'method' => 'POST',
+      'data' => drupal_http_build_query($post_data),
+      'timeout' => 15,
+      'headers' => array('Content-Type' => 'application/x-www-form-urlencoded'),
+    );
+    $endpoints = $this->getEndpoints();
+    $response = drupal_http_request($endpoints['token'], $request_options);
+    if (!isset($response->error) && $response->code == 200) {
+      $response_data = drupal_json_decode($response->data);
+      return array(
+        'id_token' => $response_data['id_token'],
+        'access_token' => $response_data['access_token'],
+        'expire' => REQUEST_TIME + $response_data['expires_in'],
+      );
+    }
+    else {
+      openid_connect_log_request_error(__FUNCTION__, $this->name, $response);
+      return FALSE;
+    }
+  }
+
+  /**
    * Overrides OpenIDConnectClientBase::retrieveUserInfo().
    */
   public function retrieveUserInfo($access_token) {
-    $endpoints = $this->getEndpoints();
-    $url = $endpoints['userinfo'];
+    // Determine if we use Openid Userinfo or Graph API.
+    switch ($this->getSetting('userinfo_graph_api_wa')) {
+      case 1:
+        $url = 'https://graph.windows.net/me?api-version=1.6';
+        $upn = 'userPrincipalName';
+        break;
+
+      default:
+        $endpoints = $this->getEndpoints();
+        $url = $endpoints['userinfo'];
+        $upn = 'upn';
+        break;
+
+    }
+
+    // Perform the request.
     $options = array(
       'method' => 'GET',
       'headers' => array(
@@ -68,19 +129,18 @@ class OpenidConnectWindowsAadClient extends OpenIDConnectClientBase {
       $userinfo = json_decode($result->data, TRUE);
     }
     else {
-      drupal_set_message(t('The UserInfo cannot be retrieved. Please check if a proper url was used.'), 'error');
+      drupal_set_message(t('The UserInfo cannot be retrieved. Please check your settings.'), 'error');
 
       return FALSE;
     }
 
     // If email is not there the user will not be created by Drupal, so we
-    // add the username as email instead, so Drupal will create it anyway.
+    // add the principal name as email instead, so Drupal will create it anyway.
     if (!isset($userinfo['email'])) {
       drupal_set_message(t('Email address not found in UserInfo. Used username instead, please check.'), 'warning');
 
-      $userinfo['email'] = $userinfo['upn'];
+      $userinfo['email'] = $userinfo[$upn];
     }
-
     return $userinfo;
   }
 
