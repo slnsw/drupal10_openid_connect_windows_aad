8fb5b7f596be6eb757951316250ec383db87b3a6
Tom van Vliet <tom.van.vliet@finalist.nl>
2016-11-17 13:13:20 +0100

    Add some more Graph options: use otherMails property and make it possible to update Drupal user email address

--- plugins/openid_connect_client/windows_aad/OpenidConnectWindowsAadClient.class.php
+++ plugins/openid_connect_client/windows_aad/OpenidConnectWindowsAadClient.class.php
@@ -39,6 +39,18 @@ class OpenidConnectWindowsAadClient extends OpenIDConnectClientBase {
       '#default_value' => $this->getSetting('userinfo_graph_api_wa'),
       '#description' => t('This option will omit the Userinfo endpoint and will use the Graph API ro retrieve the userinfo.'),
     );
+    $form['userinfo_graph_api_use_other_mails'] = array(
+      '#title' => t('Use Graph API otherMails property for email address'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('userinfo_graph_api_use_other_mails'),
+      '#description' => t('Find the first occurrence of an email address in the Graph otherMails property and use this as email address.'),
+    );
+    $form['userinfo_graph_api_update_email'] = array(
+      '#title' => t('Update email address in user profile'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('userinfo_graph_api_update_email'),
+      '#description' => t('If email address has been changed for existing user, save the new value to the user profile.'),
+    );
     $form['hide_email_address_warning'] = array(
       '#title' => t('Hide missing email address warning'),
       '#type' => 'checkbox',
@@ -48,7 +60,6 @@ class OpenidConnectWindowsAadClient extends OpenIDConnectClientBase {
 
     return $form;
   }
-
 
   /**
    * Overrides OpenIDConnectClientBase::getEndpoints().
@@ -108,21 +119,47 @@ class OpenidConnectWindowsAadClient extends OpenIDConnectClientBase {
    * @todo -- map the Graph attribute names on userinfo, as they are different.
    */
   public function retrieveUserInfo($access_token) {
-    // Determine if we use Openid Userinfo or Graph API.
+    // Determine if we use Graph API or default Openid Userinfo as this will
+    // affect the data we collect and use in the Userinfo array.
     switch ($this->getSetting('userinfo_graph_api_wa')) {
       case 1:
-        $url = 'https://graph.windows.net/me?api-version=1.6';
-        $upn = 'userPrincipalName';
+        $userinfo = $this->buildUserinfo($access_token, 'https://graph.windows.net/me?api-version=1.6', 'userPrincipalName', 'displayName');
+
+        // Check to see if we have changed email data, openid_connect doesn't
+        // give us the possibility to add a mapping for it, so we do the change
+        // now, first checking if this is wanted by checking the setting for it.
+        $user = user_load_by_name($userinfo['name']);
+        if ($user && ($user->mail <> $userinfo['email'])) {
+          $edit = array('mail' => $userinfo['email']);
+          user_save($user, $edit);
+        }
         break;
 
       default:
         $endpoints = $this->getEndpoints();
-        $url = $endpoints['userinfo'];
-        $upn = 'upn';
+        $userinfo = $this->buildUserinfo($access_token, $endpoints['userinfo'], 'upn', 'name');
         break;
-
     }
 
+    return $userinfo;
+  }
+
+  /**
+   * Helper function to do the call to the endpoint and build userinfo array.
+   *
+   * @param string $access_token
+   *   The access token.
+   * @param string $url
+   *   The endpoint we want to send the request to.
+   * @param string $upn
+   *   The name of the property that holds the Azure username.
+   * @param string $name
+   *   The name of the property we want to map to Drupal username.
+   *
+   * @return array
+   *   The userinfo array or FALSE.
+   */
+  private function buildUserinfo($access_token, $url, $upn, $name) {
     // Perform the request.
     $options = array(
       'method' => 'GET',
@@ -134,7 +171,37 @@ class OpenidConnectWindowsAadClient extends OpenIDConnectClientBase {
     $result = drupal_http_request($url, $options);
 
     if (in_array($result->code, array(200, 304))) {
-      $userinfo = json_decode($result->data, TRUE);
+      $profile_data = json_decode($result->data, TRUE);
+
+      $profile_data['name'] = $profile_data[$name];
+
+      if (!isset($profile_data['email'])) {
+        // See if we have the Graph otherMails property and use it if available,
+        // if not, add the principal name as email instead, so Drupal still will
+        // create the user anyway.
+        if ($this->getSetting('userinfo_graph_api_use_other_mails') == 1) {
+          if (!empty($profile_data['otherMails'])) {
+            // Use first occurrence of otherMails attribute.
+            $profile_data['email'] = current($profile_data['otherMails']);
+          }
+        }
+        else {
+          // Show message to user.
+          if ($this->getSetting('hide_email_address_warning') <> 1) {
+            drupal_set_message(t('Email address not found in UserInfo. Used username instead, please check this in your profile.'), 'warning');
+          }
+          // Write watchdog warning.
+          $type = 'warning';
+          $message = 'Email address of user @user not found in UserInfo. Used username instead, please check.';
+          $variables = array('@user' => $profile_data[$upn]);
+
+          watchdog($type, $message, $variables);
+
+          $profile_data['email'] = $profile_data[$upn];
+        }
+      }
+
+      return $profile_data;
     }
     else {
       drupal_set_message(t('The UserInfo cannot be retrieved. Please check your settings.'), 'error');
@@ -142,29 +209,6 @@ class OpenidConnectWindowsAadClient extends OpenIDConnectClientBase {
       return FALSE;
     }
 
-    // If email is not there the user will not be created by Drupal, so we
-    // add the principal name as email instead, so Drupal will create it anyway.
-    if (!isset($userinfo['email'])) {
-      // Show message to user.
-      if ($this->getSetting('hide_email_address_warning') <> 1) {
-        drupal_set_message(t('Email address not found in UserInfo. Used username instead, please check this in your profile.'), 'warning');
-      }
-      // Write watchdog warning.
-      $type = 'warning';
-      $message = 'Email address of user @user not found in UserInfo. Used username instead, please check.';
-      $variables = array('@user' => $userinfo[$upn]);
-
-      watchdog($type, $message, $variables);
-
-      $userinfo['email'] = $userinfo[$upn];
-    }
-    // Graph returns displayName instead of name.
-    // @todo: better solution for different attribute names.
-    if (!isset($userinfo['name'])) {
-      $userinfo['name'] = $userinfo['displayName'];
-    }
-
-    return $userinfo;
   }
 
 }
